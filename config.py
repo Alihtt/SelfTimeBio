d5033f6ff66e5cacb8bebaa0d43eaa9bfe448551
Alihtt <Aliht.workspace@gmail.com>
2023-12-26 16:16:02 +0330

    initial commit

---
+++ config.py
@@ -0,0 +1,3 @@
+api_hash = "API HASH"  # Your account API Hash
+api_id = 12345678  # Your account API ID
+phone_number = '+123456789'  # Your account number with country code
---
+++ main.py
@@ -0,0 +1,35 @@
+import asyncio
+import datetime
+from pyrogram import Client
+from config import api_id, api_hash, phone_number
+from pyrogram.errors.exceptions import FloodWait
+import logging
+
+
+async def main():
+    async with Client(name="my_account",
+                      api_id=api_id,
+                      api_hash=api_hash,
+                      phone_number=phone_number) as app:
+
+        while True:
+            n = datetime.datetime.now()
+            if n.second == 0:
+                now_str = "⏱ Now: " + datetime.datetime.now().strftime('%H:%M') + " ⏱"
+                logging.info(f'Time is {now_str}')
+                try:
+                    logging.info(f'Trying to updated Profile')
+                    await app.update_profile(bio=now_str)
+                    logging.info(f'Profile updated: {now_str}')
+                except FloodWait as e:
+                    logging.warning(f'Flood waited for {e.value}', exc_info=True)
+                    await asyncio.sleep(e.value + 1)
+            await asyncio.sleep(1)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s',
+                        datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
+    logging.info('Bot started...')
+    print('Bot started...')
+    asyncio.run(main())
